7efce5bab1d52fc00f48264dbd3b98c09eb2a0a3
King_Raccoon <zvezdakov2015@yandex.ru>
2020-05-07 18:36:50 +0700

    asda

--- app/src/main/java/com/castprogramm/investgame/MainActivity.kt
+++ app/src/main/java/com/castprogramm/investgame/MainActivity.kt
@@ -2,11 +2,34 @@ package com.castprogramm.investgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var X = 0
+        var Y = 0
+        var point : DataPoint
+        var exampleSeries = findViewById<View>(R.id.graph) as GraphView
+        var test = Stock()
+        var plotPoints = arrayOfNulls<DataPoint>(3)
+        for (i in 0..2){
+            X = i
+            Y = 2*i
+            point = DataPoint(X.toDouble(), Y.toDouble())
+            plotPoints[i] = point
+        }
+        val functiom = LineGraphSeries<DataPoint>(plotPoints)
+
+        graph.viewport.setScalable(true)
+        graph.viewport.setScalableY(true)
+
+        graph.addSeries(functiom)
     }
 }
--- app/src/main/java/com/castprogramm/investgame/Stock.kt
+++ app/src/main/java/com/castprogramm/investgame/Stock.kt
@@ -1,4 +1,13 @@
 package com.castprogramm.investgame
 
-class Stock {
+import android.util.ArrayMap
+import java.util.*
+
+class Stock: Up {
+    var name: String? = null
+    var cost: Double? = null
+    var costsofStock : MutableMap<String, String> = mutableMapOf()
+    override fun update() {
+        costsofStock.put(Date().toString(), (0..1000).random().toString())
+    }
 }
