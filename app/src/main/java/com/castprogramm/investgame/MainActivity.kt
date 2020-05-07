bfccfd523e5a36770d292ca6ce132235cb089036
Arbonik <zmear@mail.ru>
2020-05-07 13:59:34 +0300

    Test Up demonstration

--- app/src/main/java/com/castprogramm/investgame/MainActivity.kt
+++ app/src/main/java/com/castprogramm/investgame/MainActivity.kt
@@ -2,11 +2,27 @@ package com.castprogramm.investgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
+class Test(var text : TextView) : Up{
+    var test = 0;
+    override fun update() {
+        test ++
+        text.setText(test.toString())
+    }
+}
 class MainActivity : AppCompatActivity() {
+    var handler = Handler()
+    var testing = Updater(handler)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var wow = Test(value)
+        testing.objectsToUpdate.add(wow)
+    handler.post(testing)
+
     }
 }
