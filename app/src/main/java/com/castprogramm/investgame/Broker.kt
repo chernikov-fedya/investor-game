2b5c270c8cb70d1cec0b577170b5bbc19f162318
User <fdf@mail.ru>
2020-05-07 17:14:09 +0700

    compan

--- app/src/main/java/com/castprogramm/investgame/Broker.kt
+++ app/src/main/java/com/castprogramm/investgame/Broker.kt
@@ -1,6 +1,6 @@
 package com.castprogramm.investgame
 
 class Broker {
-    var wallet: Double? = null
+    var wallet: Double? = null // сколько есть денег у брокера
     var myStock: MutableList<Stock>? = null
 }
