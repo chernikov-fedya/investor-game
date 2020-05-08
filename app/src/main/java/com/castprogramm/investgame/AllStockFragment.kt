652e60df3e00fdd0a46efdba67ec8fdd39fbbef1
KingOfRaccoon <zvezdakov2015@yandex.ru>
2020-05-08 20:58:46 +0700

    asda

---
+++ app/src/main/java/com/castprogramm/investgame/AllStockFragment.kt
@@ -0,0 +1,35 @@
+package com.castprogramm.investgame
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class AllStockFragment: Fragment() {
+    companion object{
+        fun newInstance(excer: MutableList<Stock>): AllStockFragment {
+            var temp = AllStockFragment()
+            temp.recStocks = excer
+            return temp
+        }
+    }
+    var recStocks: MutableList<Stock> =  mutableListOf()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var ret = inflater.inflate(R.layout.fragment_all_stock, container, false)
+        var recycler : RecyclerView = ret.findViewById(R.id.aleksey)
+        recycler.adapter = StockAdapter().apply {
+            stocks = recStocks
+        }
+
+        var pi = LinearLayoutManager(ret.context)
+        recycler.layoutManager = pi
+        return ret
+    }
+}
---
+++ app/src/main/java/com/castprogramm/investgame/Stoks.kt
@@ -0,0 +1,12 @@
+package com.castprogramm.investgame
+
+object Stoks {
+    var stocks: MutableList<Stock> = mutableListOf(
+        Stock().apply { name = "Акции Fitness-Project" },
+        Stock().apply { name = "Акции Google" },
+        Stock().apply { name = "Акции Apple" },
+        Stock().apply { name = "Акции Samsung" },
+        Stock().apply { name = "Акции Xiaomi" },
+        Stock().apply { name = "Акции InvesterGame" },
+        Stock().apply { name = "Акции Huawei" })
+}
