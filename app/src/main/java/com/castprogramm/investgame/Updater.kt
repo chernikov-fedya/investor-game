a8eec573812ef07c3e57ca18ef35895e94b071f9
KingOfRaccoon <zvezdakov2015@yandex.ru>
2020-05-11 00:47:12 +0700

    asda

--- app/src/main/java/com/castprogramm/investgame/Updater.kt
+++ app/src/main/java/com/castprogramm/investgame/Updater.kt
@@ -4,7 +4,7 @@ import android.os.Handler
 
 // класс для обновления состояния всех объектов
 class Updater(var handler: Handler, var UPDATE_TIME : Long = 5000) : Runnable{
-
+    var play: Boolean = true
     var objectsToUpdate : MutableList<Up> = mutableListOf()
     var timePoint : Long // нужна для расчета врмени которое прошло с предыдущего обновления
     var runFlag = true
@@ -12,6 +12,7 @@ class Updater(var handler: Handler, var UPDATE_TIME : Long = 5000) : Runnable{
         timePoint = System.currentTimeMillis()
     }
     override fun run() {
+        if (play == true){
             var nowTime = System.currentTimeMillis()
             if (nowTime - timePoint > UPDATE_TIME) {
                 objectsToUpdate.forEach { it.update() }
@@ -19,6 +20,11 @@ class Updater(var handler: Handler, var UPDATE_TIME : Long = 5000) : Runnable{
             }
         if (runFlag)
             handler.postDelayed(this, UPDATE_TIME)
+        }
+        else
+        {
+
+        }
     }
 }
 
