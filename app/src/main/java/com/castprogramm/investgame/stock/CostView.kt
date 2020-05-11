8b12cf6f886dffc947cbd09459399bd1cbc56b4e
KingOfRaccoon <zvezdakov2015@yandex.ru>
2020-05-11 21:48:18 +0700

    asda

--- app/src/main/java/com/castprogramm/investgame/stock/CostView.kt
+++ app/src/main/java/com/castprogramm/investgame/stock/CostView.kt
@@ -11,23 +11,23 @@ import com.jjoe64.graphview.series.LineGraphSeries
 // Класс отрисовки графика, наследуемый от GraphView, с адапртацией под класс Stock
 class CostView(context: Context, atrr: AttributeSet): GraphView(context, atrr), Up {
     init {
-        this.viewport.isScalable = true // Добавление возмозможности настройки масштаба
+        this.viewport.setScalable(true) // Добавление возмозможности настройки масштаба по оси X и Y
         this.viewport.setScalableY(true)
     }
-    fun addStock(temp: Stock, live:LifecycleOwner){
-        var newtest : LiveData<MutableList<DataPoint>> = temp.costsofStock
-        newtest.observe(live, androidx.lifecycle.Observer { t ->
-            var functiom = LineGraphSeries<DataPoint>(Array(t.size, { t[it] }))
-            this.addSeries(functiom)
-            this.viewport.scrollToEnd()
-            this.viewport.computeScroll()
-            this.viewport.setMinX(0.0)
+    // Функция для добавления DataPoint (серии значений (Date, Double) или (Double, Double)) в график
+    fun addStock(stock: Stock, live:LifecycleOwner){
+        var newPoint : LiveData<MutableList<DataPoint>> = stock.costsofStock // Копия LiveData из переменной stock
+        newPoint.observe(live, androidx.lifecycle.Observer { t -> // С помощью паттерна Observer (наблюдатель) при обновлении значения в LiveData будет выполняться идущий ниже код
+            var functiom = LineGraphSeries<DataPoint>(Array(t.size, { t[it] })) // Создание серии значений
+            this.addSeries(functiom) // Добавление серии значений в график
+            this.viewport.scrollToEnd() // Прокрутка графика в масимальное значение оси X
+            this.viewport.setMinX(0.0) // Установление минимального значения на оси Х
         })
     }
 
-    override fun update() {
-        this.viewport.scrollToEnd()
-        this.viewport.setMinX(0.0)
-        this.viewport.setMinY(0.0)
+    override fun update() { // Переопределение функции из интерфейса Up
+        this.viewport.scrollToEnd() // Прокрутка графика в масимальное значение оси X
+        this.viewport.setMinX(0.0) // Установление минимального значения на оси Х
+        this.viewport.setMinY(0.0) // Установление минимального значения на оси Y
     }
 }
